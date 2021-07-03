f5f8ee8d3fad4acb7600d568846fb1325643f006
delevoye <delevoye.guillaume@gmail.com>
2021-07-03 17:50:06 +0200

    v0.1 ?

--- pyckage/resources/NAME/NAME/__init__.py
+++ pyckage/resources/NAME/NAME/__init__.py
@@ -4,4 +4,4 @@ import [NAME].app_module1
 ######## DEFINITION OF PATH TO RESOURCES
 import os
 HERE = os.path.dirname(__file__)
-[NAME]_RESOURCES_DIR = os.path.join(HERE, "resources")
+RESOURCES_DIR = os.path.join(HERE, "resources")
